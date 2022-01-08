316de1069e98c9c7fa9a47a84bc5b2df38fdd38e
Gokul <gokulchandan18@gmail.com>
2022-01-08 09:37:08 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { UsdinrPipe } from './pipes/usdinr.pipe';
-import { MyCustomPurePipePipe } from './my-custom-pure-pipe.pipe';
-import { MyCustomImpurePipePipe } from './my-custom-impure-pipe.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    UsdinrPipe,
-    MyCustomPurePipePipe,
-    MyCustomImpurePipePipe
+    UsdinrPipe
   ],
   imports: [
     BrowserModule
